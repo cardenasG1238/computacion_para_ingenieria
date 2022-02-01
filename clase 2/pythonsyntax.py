bc3196505cbd93d7cec1951f0be5bdca5c14d804
Your Name <you@example.com>
2022-02-01 08:19:09 -0400

    add clase 2

---
+++ clase 2/pythonsyntax.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# comentarios en line
+"""
+comentarios en bloques o multilinea
+linea 1
+linea 2
+"""
+# crear una constante
+
+NAME = "jhesvid"
+FULL_NAME = 'jhesvid cardenas garcia'
+
+print (NAME)
+print (FULL_NAME)
+
+# tipos de datos
+entero = 90
+print (f'el entero tiene valor de {entero}')
+decimal = 6.14
+print (f'el valor del decimal es {decimal}')
+caracter = 'j'
+print (f'el valor del decimal es {caracter}')
+cadena = 'primera cadena'
+print (f'el valor del decimal es {cadena}')
+cadena_2 = 'segunda cadena'
+print (f'el valor del decimal es {cadena_2}')
+
+booleano = False # valores true o False
+print (f'el valor del decimal es {booleano}')
+# lista
+list =[1, 2, 3, 4]
+print=(f'el valor de la lista es : {list}')
+# diccionario
+diccionario = {'nombre': 'jhesvid cardenas', "edad": 22}
+nombre = diccionario ["nombre"]
+edad = diccionario ["edad"]
+print (f'el nombre es : {nombre} y su edad es : {edad}')
+
+# entradas estandar
+
+telefono = input ("ingrese el numero telefonico")
+# cast de tipos
+a = int(input("Ingrese a :"))
+b = int(input("Ingrese b :"))
+print (f'el resultado de sumar a+b = {a+b}')
+ 
+ # estructuras de control
+ 
